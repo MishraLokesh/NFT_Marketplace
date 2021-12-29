3d572393e3992d670b3771d63d36999e4087ec8c
MishraLokesh <4man.mishra@gmail.com>
2021-12-29 20:56:03 +0530

    test cases successfully passing

--- contracts/Color.sol
+++ contracts/Color.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyCollectible is ERC721 {
+contract Color is ERC721 {
     constructor() ERC721("Color", "COLOR"){
     }
 }
